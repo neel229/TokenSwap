35a3decfa9ec07c43b32358395b1dd310caa3841
neel229 <device.neel@gmail.com>
2020-12-23 19:20:46 +0530

    DemoToken minted and transferred to exchange address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
 const TokenSwap = artifacts.require("TokenSwap");
+const DemoToken = artifacts.require("DemoToken");
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenSwap);
+module.exports = async function(deployer) {
+  // Deploy DemoToken smart contract on the blockchain
+  await deployer.deploy(DemoToken);
+  const token = await DemoToken.deployed();
+
+  // Deploy the TokenSwap smart contract on the blockchain.
+  await deployer.deploy(TokenSwap);
+  const tokenSwap = await TokenSwap.deployed();
+
+  // Transfer all the tokens to the exchange address
+  await token.transfer(tokenSwap.address, "1000000000000000000000000");
 };
